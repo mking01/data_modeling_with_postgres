a1d43c06151f395dac1189e5bfee35a2488d7c76
mking01 <59142310+mking01@users.noreply.github.com>
2020-01-31 11:03:14 -0600

    Update sql_queries.py
    Added NOT NULL requirement to some table columns

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,35 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay 
-                                (songplay_id int PRIMARY KEY, 
+                                (songplay_id SERIAL NOT NULL PRIMARY KEY, 
                                 start_time date REFERENCES time(start_time), 
                                 user_id int NOT NULL REFERENCES users(user_id), 
                                 level varchar, 
-                                song_id varchar REFERENCES songs(song_id), 
-                                artist_id varchar REFERENCES artists(artist_id), 
+                                song_id varchar NOT NULL REFERENCES songs(song_id), 
+                                artist_id varchar NOT NULL REFERENCES artists(artist_id), 
                                 session_id int, 
                                 location varchar, 
                                 user_agent varchar)
                                 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users 
-                            (user_id int PRIMARY KEY, 
-                            first_name varchar, 
-                            last_name varchar, 
+                            (user_id int NOT NULL PRIMARY KEY, 
+                            first_name varchar NOT NULL, 
+                            last_name varchar NOT NULL, 
                             gender varchar, \
                             level varchar)
                             """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs 
-                            (song_id varchar PRIMARY KEY, 
+                            (song_id varchar NOT NULL PRIMARY KEY, 
                             title varchar, 
-                            artist_id varchar REFERENCES artists(artist_id), 
+                            artist_id varchar NOT NULL REFERENCES artists(artist_id), 
                             year int, 
                             duration float)
                             """)
 
 artist_table_create = ("""CREATE TABLE artists 
-                            (artist_id varchar PRIMARY KEY, 
+                            (artist_id varchar NOT NULL PRIMARY KEY, 
                             name varchar, 
                             location varchar, 
                             latitude float, 
@@ -43,7 +43,7 @@ artist_table_create = ("""CREATE TABLE artists
                             """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time 
-                            (start_time date PRIMARY KEY, 
+                            (start_time date NOT NULL PRIMARY KEY, 
                             hour int, 
                             day int, 
                             week int, 
@@ -61,7 +61,7 @@ songplay_table_insert = ("""INSERT INTO songplay
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
                         VALUES (%s, %s, %s, %s, %s)
-                        ON CONFLICT (user_id) DO NOTHING;
+                        ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
                     """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
@@ -81,7 +81,12 @@ time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, ye
                     """)
 
 # FIND SONGS
-song_select = ("""select * from songplay where song_id IS NOT NULL and artist_id IS NOT NULL""")
+song_select = ("""SELECT s.song_id, a.artist_id
+                    FROM songs as s
+                    JOIN artists as a ON s.artist_id = a.artist_id
+                    WHERE s.title = %s
+                    AND a.name = %s
+                    AND s.duration = %s""")
 
 # QUERY LISTS
 create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
